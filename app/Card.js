b3679443dfa804e770fab3af4025e339abe4d068
arkdru23 <arkadiusz.drukala@gmail.com>
2021-01-23 11:06:37 +0100

    add types and weight to const

--- app/Card.js
+++ app/Card.js
@@ -1,3 +1,21 @@
+export const WEIGHTS = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A",
+];
+
+export const TYPES = ["spades", "hearts", "diamonds", "clubs"];
+
 export class Card {
   mapTextToSign = {
     hearts: "&hearts;",
